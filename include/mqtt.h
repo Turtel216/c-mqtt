9ce49598be1869c6c73c7714e85d52b74eb4145a
Turtel216 <papadimitrios216@gmail.com>
2025-04-07 14:40:50 +0300

    Add MQTT CONNECT and CONNACK

---
+++ include/mqtt.h
@@ -0,0 +1,90 @@
+#ifndef MQTT_H_
+#define MQTT_H_
+
+#include <stdio.h>
+
+// Fixed size ot MQTT fixed header
+#define HEADER_LEN 2
+// Fixed size of every MQTT ACK
+#define ACK_LEN 4
+
+// Stub bytes
+#define CONNACK_BYTE 0x20
+#define PUBLISH_BYTE 0x30
+#define PUBACK_BYTE 0x40
+#define PUBREC_BYTE 0x50
+#define PUBREL_BYTE 0x60
+#define PUBCOMP_BYTE 0x70
+#define SUBACK_BYTE 0x90
+#define UNSUBACK_BYTE 0xB0
+#define PINGRESP_BYTE 0xD0
+
+// Message types
+enum packet_type {
+	CONNECT = 1,
+	CONNACK,
+	PUBLISH,
+	PUBACK,
+	PUBREC,
+	PUBREL,
+	PUBCOMP,
+	SUBSCRIBE,
+	SUBACK,
+	UNSUBSCRIBE,
+	UNSUBACK,
+	PINGREQ,
+	PINGRESP,
+	DISCONNECT
+};
+
+enum qos_level { AT_MOST_ONCE, AT_LEAST_ONCE, EXACTLY_ONCE };
+
+// Byte representation of an MQTT header
+union mqtt_header {
+	unsigned char byte;
+	struct {
+		unsigned retain : 1;
+		unsigned qos : 2;
+		unsigned dup : 1;
+		unsigned type : 4;
+	} bits;
+};
+
+// Struct representing an MQTT CONNECT request
+struct mqtt_connect {
+	union mqtt_header header; // MQTT header
+	union {
+		unsigned char byte;
+		struct { // Byte describing payload
+			int reserved : 1;
+			unsigned clean_session : 1;
+			unsigned will : 1;
+			unsigned will_qos : 2;
+			unsigned will_retain : 1;
+			unsigned password : 1;
+			unsigned username : 1;
+		} bits;
+		struct { // Struct representaion of payload
+			unsigned short keepalive;
+			unsigned char *client_id;
+			unsigned char *username;
+			unsigned char *password;
+			unsigned char *will_topic;
+			unsigned char *will_message;
+		} payload;
+	};
+};
+
+// Struct representing an MQTT CONNACT response
+struct mqtt_connack {
+	union mqtt_header header; // MQTT Header
+	union {
+		unsigned char byte;
+		struct {
+			unsigned session_present : 1;
+			unsigned reserved : 2;
+		} bits;
+		unsigned char rc;
+	}
+};
+#endif // MQTT_H_
