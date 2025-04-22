6daba2a05bd48185209f1e7ff78425d94a65d8a3
Turtel216 <papadimitrios216@gmail.com>
2025-04-22 23:57:29 +0300

    Update mqtt documentation

--- include/mqtt.h
+++ include/mqtt.h
@@ -1,4 +1,7 @@
-// The MQTT module defines utilities for handling MQTT communications
+/**
+ * @file mqtt.h
+ * @brief The MQTT module defines utilities for handling MQTT communications
+ */
 
 #ifndef MQTT_H_
 #define MQTT_H_
@@ -6,192 +9,307 @@
 #include <stddef.h>
 #include <stdio.h>
 
-// Fixed size ot MQTT fixed header
+/** @name MQTT Header Constants */
+/**@{*/
+/** Fixed size of MQTT fixed header */
 #define HEADER_LEN 2
-// Fixed size of every MQTT ACK
+/** Fixed size of every MQTT ACK */
 #define ACK_LEN 4
+/**@}*/
 
-// Stub bytes
+/** @name MQTT Message Type Bytes */
+/**@{*/
+/** CONNACK message type byte */
 #define CONNACK_BYTE 0x20
+/** PUBLISH message type byte */
 #define PUBLISH_BYTE 0x30
+/** PUBACK message type byte */
 #define PUBACK_BYTE 0x40
+/** PUBREC message type byte */
 #define PUBREC_BYTE 0x50
+/** PUBREL message type byte */
 #define PUBREL_BYTE 0x60
+/** PUBCOMP message type byte */
 #define PUBCOMP_BYTE 0x70
+/** SUBACK message type byte */
 #define SUBACK_BYTE 0x90
+/** UNSUBACK message type byte */
 #define UNSUBACK_BYTE 0xB0
+/** PINGRESP message type byte */
 #define PINGRESP_BYTE 0xD0
+/**@}*/
 
-// Message types
+/**
+ * @brief MQTT message types as defined in the MQTT protocol
+ */
 enum packet_type {
-	CONNECT = 1,
-	CONNACK,
-	PUBLISH,
-	PUBACK,
-	PUBREC,
-	PUBREL,
-	PUBCOMP,
-	SUBSCRIBE,
-	SUBACK,
-	UNSUBSCRIBE,
-	UNSUBACK,
-	PINGREQ,
-	PINGRESP,
-	DISCONNECT
+	CONNECT = 1,    /**< Client request to connect to server */
+	CONNACK,        /**< Server acknowledgment of connection */
+	PUBLISH,        /**< Publish message */
+	PUBACK,         /**< Publish acknowledgment */
+	PUBREC,         /**< Publish received (QoS 2) */
+	PUBREL,         /**< Publish release (QoS 2) */
+	PUBCOMP,        /**< Publish complete (QoS 2) */
+	SUBSCRIBE,      /**< Client subscribe request */
+	SUBACK,         /**< Server subscribe acknowledgment */
+	UNSUBSCRIBE,    /**< Client unsubscribe request */
+	UNSUBACK,       /**< Server unsubscribe acknowledgment */
+	PINGREQ,        /**< Client ping request */
+	PINGRESP,       /**< Server ping response */
+	DISCONNECT      /**< Client disconnection notification */
 };
 
-enum qos_level { AT_MOST_ONCE, AT_LEAST_ONCE, EXACTLY_ONCE };
+/**
+ * @brief Quality of Service levels for MQTT messaging
+ */
+enum qos_level {
+	AT_MOST_ONCE,   /**< QoS 0: Message delivered at most once (fire and forget) */
+	AT_LEAST_ONCE,  /**< QoS 1: Message delivered at least once (acknowledged delivery) */
+	EXACTLY_ONCE    /**< QoS 2: Message delivered exactly once (assured delivery) */
+};
 
-// Byte representation of an MQTT header
+/**
+ * @brief Byte representation of an MQTT header
+ */
 union mqtt_header {
-	unsigned char byte;
+	unsigned char byte;           /**< Raw byte value */
 	struct {
-		unsigned retain : 1;
-		unsigned qos : 2;
-		unsigned dup : 1;
-		unsigned type : 4;
+		unsigned retain : 1;  /**< Retain flag */
+		unsigned qos : 2;     /**< Quality of Service level */
+		unsigned dup : 1;     /**< Duplicate delivery flag */
+		unsigned type : 4;    /**< Message type */
 	} bits;
 };
 
-// MQTT CONNECT request
+/**
+ * @brief MQTT CONNECT request structure
+ */
 struct mqtt_connect {
-	union mqtt_header header; // MQTT header
+	union mqtt_header header;     /**< MQTT header */
 	union {
-		unsigned char byte;
-		struct { // Byte describing payload
+		unsigned char byte;   /**< Raw byte value */
+		struct {              /**< Byte describing payload */
 			int reserved : 1;
-			unsigned clean_session : 1;
-			unsigned will : 1;
-			unsigned will_qos : 2;
-			unsigned will_retain : 1;
-			unsigned password : 1;
-			unsigned username : 1;
+			unsigned clean_session : 1;  /**< Clean session flag */
+			unsigned will : 1;           /**< Will flag */
+			unsigned will_qos : 2;       /**< Will QoS level */
+			unsigned will_retain : 1;    /**< Will retain flag */
+			unsigned password : 1;       /**< Password flag */
+			unsigned username : 1;       /**< Username flag */
 		} bits;
-		struct { // Struct representaion of payload
-			unsigned short keepalive;
-			unsigned char *client_id;
-			unsigned char *username;
-			unsigned char *password;
-			unsigned char *will_topic;
-			unsigned char *will_message;
+		struct {              /**< Struct representation of payload */
+			unsigned short keepalive;     /**< Keep-alive timer value in seconds */
+			unsigned char *client_id;     /**< Client identifier */
+			unsigned char *username;      /**< Username if username flag set */
+			unsigned char *password;      /**< Password if password flag set */
+			unsigned char *will_topic;    /**< Will topic if will flag set */
+			unsigned char *will_message;  /**< Will message if will flag set */
 		} payload;
 	};
 };
 
-// MQTT CONNACK response
+/**
+ * @brief MQTT CONNACK response structure
+ */
 struct mqtt_connack {
-	union mqtt_header header; // MQTT Header
+	union mqtt_header header;     /**< MQTT Header */
 	union {
-		unsigned char byte;
+		unsigned char byte;   /**< Raw byte value */
 		struct {
-			unsigned session_present : 1;
-			unsigned reserved : 2;
+			unsigned session_present : 1;  /**< Session present flag */
+			unsigned reserved : 2;         /**< Reserved bits */
 		} bits;
-		unsigned char rc; // Return Code
+		unsigned char rc;     /**< Return Code */
 	};
 };
 
-// MQTT SUBSCRIBE request
+/**
+ * @brief MQTT SUBSCRIBE request structure
+ */
 struct mqtt_subscribe {
-	union mqtt_header header;
-	unsigned short pkt_id; // Package id
-	unsigned short tuples_len;
+	union mqtt_header header;     /**< MQTT header */
+	unsigned short pkt_id;        /**< Package id */
+	unsigned short tuples_len;    /**< Number of topic tuples */
 	struct {
-		unsigned short topic_len; // Length of topic string
-		unsigned char *topic; // Topic String
-		unsigned qos;
-	} *tuples;
+		unsigned short topic_len;  /**< Length of topic string */
+		unsigned char *topic;      /**< Topic String */
+		unsigned qos;              /**< Requested QoS level */
+	} *tuples;                    /**< Array of topic tuples */
 };
 
-// MQTT UNSUBSCRIBE request
+/**
+ * @brief MQTT UNSUBSCRIBE request structure
+ */
 struct mqtt_unsubscribe {
-	union mqtt_header header;
-	unsigned short pkt_id; // Package id
-	unsigned short tuples_len;
+	union mqtt_header header;     /**< MQTT header */
+	unsigned short pkt_id;        /**< Package id */
+	unsigned short tuples_len;    /**< Number of topic tuples */
 	struct {
-		unsigned short topic_len; // Length of topic string
-		unsigned char *topic; // Topic String
-	} *tuples;
+		unsigned short topic_len;  /**< Length of topic string */
+		unsigned char *topic;      /**< Topic String */
+	} *tuples;                    /**< Array of topic tuples */
 };
 
-// Packet definition for SUBSCRIBE and PUBLISH
+/**
+ * @brief Packet definition for SUBSCRIBE and PUBLISH acknowledgments
+ */
 struct mqtt_suback {
-	union mqtt_header header;
-	unsigned short pkt_id; // Package id
-	unsigned short rcslen; // Return Code String length
-	unsigned char *rcs; // Return Code String
+	union mqtt_header header;     /**< MQTT header */
+	unsigned short pkt_id;        /**< Package id */
+	unsigned short rcslen;        /**< Return Code String length */
+	unsigned char *rcs;           /**< Return Code String */
 };
 
-// MQTT PUBLISH request
+/**
+ * @brief MQTT PUBLISH request structure
+ */
 struct mqtt_publish {
-	union mqtt_header header;
-	unsigned short pkt_id; // Package id
-	unsigned short topiclen; // Topic String length
-	unsigned char *topic; // Topic String
-	unsigned short payloadlen; // Payload String length
-	unsigned char *payload; // Payload String
+	union mqtt_header header;     /**< MQTT header */
+	unsigned short pkt_id;        /**< Package id */
+	unsigned short topiclen;      /**< Topic String length */
+	unsigned char *topic;         /**< Topic String */
+	unsigned short payloadlen;    /**< Payload String length */
+	unsigned char *payload;       /**< Payload String */
 };
 
-// Generic MQTT packet
+/**
+ * @brief Generic MQTT acknowledgment packet structure
+ */
 struct mqtt_ack {
-	union mqtt_header header;
-	unsigned short pkt_id; // Package id
+	union mqtt_header header;     /**< MQTT header */
+	unsigned short pkt_id;        /**< Package id */
 };
 
-// ACK packets for:
-// PUBACK
+/** @name MQTT specialized acknowledgment packet types */
+/**@{*/
+/** PUBACK packet type */
 typedef struct mqtt_ack mqtt_puback;
-// PUBREC
+/** PUBREC packet type */
 typedef struct mqtt_ack mqtt_pubrec;
-// PUBREL
+/** PUBREL packet type */
 typedef struct mqtt_ack mqtt_pubrel;
-// PUBCOMP
+/** PUBCOMP packet type */
 typedef struct mqtt_ack mqtt_pubcomp;
-// UNSUBACK
+/** UNSUBACK packet type */
 typedef struct mqtt_ack mqtt_unsuback;
-// PINGREQ
+/** PINGREQ packet type */
 typedef union mqtt_header mqtt_pingreq;
-// PINGRESP
+/** PINGRESP packet type */
 typedef union mqtt_header mqtt_pingresp;
-// DISCONNECT
+/** DISCONNECT packet type */
 typedef union mqtt_header mqtt_disconnect;
+/**@}*/
 
-// MQTT packet
+/**
+ * @brief MQTT packet union for representing any MQTT packet type
+ */
 union mqtt_packet {
-	struct mqtt_ack ack;
-	union mqtt_header header;
-	struct mqtt_connect connect;
-	struct mqtt_connack connack;
-	struct mqtt_suback suback;
-	struct mqtt_publish publish;
-	struct mqtt_subscribe subscribe;
-	struct mqtt_unsubscribe unsubscribe;
+	struct mqtt_ack ack;            /**< Generic acknowledgment structure */
+	union mqtt_header header;       /**< MQTT header */
+	struct mqtt_connect connect;    /**< CONNECT packet */
+	struct mqtt_connack connack;    /**< CONNACK packet */
+	struct mqtt_suback suback;      /**< SUBACK packet */
+	struct mqtt_publish publish;    /**< PUBLISH packet */
+	struct mqtt_subscribe subscribe;    /**< SUBSCRIBE packet */
+	struct mqtt_unsubscribe unsubscribe;  /**< UNSUBSCRIBE packet */
 };
 
-// Returns the length of an encoded mqtt byte stream
+/**
+ * @brief Encodes the Remaining Length field of an MQTT packet header
+ *
+ * @param[out] buf Buffer to store the encoded length
+ * @param[in] len Length value to encode
+ * @return Number of bytes used for encoding the length
+ */
 int mqtt_encode_length(unsigned char *, size_t);
-// Returns the length of an decoded mqtt byte stream
+
+/**
+ * @brief Decodes the Remaining Length field of an MQTT packet header
+ *
+ * @param[in,out] buf Pointer to the buffer containing the encoded length
+ * @return Decoded length value
+ */
 unsigned long long mqtt_decode_length(const unsigned char **);
-// Marshals an mqtt packet
+
+/**
+ * @brief Unmarshals an MQTT packet
+ *
+ * @param[in] packet Pointer to the MQTT packet to unmarshal
+ * @param[in] type Type of the MQTT packet
+ * @return Status code indicating success or failure
+ */
 int unpack_mqtt_packet(const union mqtt_packet *, unsigned);
-// Unmarshals an mqtt packet
+
+/**
+ * @brief Marshals an MQTT packet
+ *
+ * @param[in] packet Pointer to the MQTT packet to marshal
+ * @param[in] type Type of the MQTT packet
+ * @return Pointer to the marshaled packet buffer (must be freed by caller)
+ */
 unsigned char *packet_mqtt_packet(const union mqtt_packaet *, unsigned);
 
-// Utility function that returns the mqtt packet header
+/**
+ * @brief Utility function that creates an MQTT packet header
+ *
+ * @param[in] byte Header byte value
+ * @return Pointer to the created MQTT header
+ */
 union mqtt_header *mqtt_packet_header(unsigned char);
-// Utility function that returns the mqtt packet ACT
+
+/**
+ * @brief Utility function that creates an MQTT ACK packet
+ *
+ * @param[in] byte Header byte value
+ * @param[in] pkt_id Packet identifier
+ * @return Pointer to the created MQTT ACK packet
+ */
 struct mqtt_ack *mqtt_packet_ack(unsigned char, unsigned short);
-// Utility function that returns an mqtt CONNACK response
+
+/**
+ * @brief Utility function that creates an MQTT CONNACK response
+ *
+ * @param[in] byte Header byte value
+ * @param[in] flags Connection flags
+ * @param[in] rc Return code
+ * @return Pointer to the created MQTT CONNACK packet
+ */
 struct mqtt_connack *mqtt_packet_connack(unsigned char, unsigned char,
 					 unsigned char);
-// Utility function that returns an mqtt SUBACK request
+
+/**
+ * @brief Utility function that creates an MQTT SUBACK packet
+ *
+ * @param[in] byte Header byte value
+ * @param[in] pkt_id Packet identifier
+ * @param[in] rcs Return codes
+ * @param[in] rcslen Length of return codes
+ * @return Pointer to the created MQTT SUBACK packet
+ */
 struct mqtt_suback *mqtt_packet_suback(unsigned char, unsigned short,
 				       unsigned char *, unsigned short);
-// Utility function that returns an mqtt  request
+
+/**
+ * @brief Utility function that creates an MQTT PUBLISH packet
+ *
+ * @param[in] byte Header byte value
+ * @param[in] pkt_id Packet identifier
+ * @param[in] topiclen Length of topic
+ * @param[in] topic Topic string
+ * @param[in] payloadlen Length of payload
+ * @param[in] payload Payload data
+ * @return Pointer to the created MQTT PUBLISH packet
+ */
 struct mqtt_publish *mqtt_packet_publish(unsigned char, unsigned short, size_t,
 					 unsigned char *, size_t,
 					 unsigned char *);
-// Utility function that releases an mqtt packet and frees up its memory
+
+/**
+ * @brief Utility function that releases an MQTT packet and frees up its memory
+ *
+ * @param[in] packet Pointer to the MQTT packet to release
+ * @param[in] type Type of the MQTT packet
+ */
 void mqtt_packet_release(union mqtt_packet *, unsigned);
 
 #endif // MQTT_H_
--- src/mqtt.c
+++ src/mqtt.c
@@ -2,120 +2,216 @@
 #include "../include/pack.h"
 #include <stdlib.h>
 
-/* PRIVATE helper functions to pack and unpack MQTT packets */
-static size_t unpack_mqtt_connect(const unsigned char *, union mqtt_header *,
-				  union mqtt_packet *);
-static size_t unpack_mqtt_publish(const unsigned char *, union mqtt_header *,
-				  union mqtt_packet *);
-static size_t unpack_mqtt_subscribe(const unsigned char *, union mqtt_header *,
-				    union mqtt_packet *);
-static size_t unpack_mqtt_unsubscribe(const unsigned char *,
-				      union mqtt_header *, union mqtt_packet *);
-static size_t unpack_mqtt_ack(const unsigned char *, union mqtt_header *,
-			      union mqtt_packet *);
-static unsigned char *pack_mqtt_header(const union mqtt_header *);
-static unsigned char *pack_mqtt_ack(const union mqtt_packet *);
-static unsigned char *pack_mqtt_connack(const union mqtt_packet *);
-static unsigned char *pack_mqtt_suback(const union mqtt_packet *);
-static unsigned char *pack_mqtt_publish(const union mqtt_packet *);
-/* ######################################################## */
+/**
+ * @file mqtt.c
+ * @brief Implementation of MQTT protocol packet processing functions
+ *
+ * This file provides functions for packing and unpacking MQTT protocol packets
+ * according to the MQTT v3.1.1 specification. It includes utilities for length
+ * encoding/decoding and packet-specific serialization/deserialization.
+ */
 
-/*
- * MQTT v3.1.1 standard, Remaining length field on the fixed header can be at
+/** @name Private helper functions for packing and unpacking MQTT packets */
+/**@{*/
+/**
+ * @brief Unpacks an MQTT CONNECT packet from a byte buffer
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
+static size_t unpack_mqtt_connect(const unsigned char *, union mqtt_header *,
+                                 union mqtt_packet *);
+
+/**
+ * @brief Unpacks an MQTT PUBLISH packet from a byte buffer
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
+static size_t unpack_mqtt_publish(const unsigned char *, union mqtt_header *,
+                                 union mqtt_packet *);
+
+/**
+ * @brief Unpacks an MQTT SUBSCRIBE packet from a byte buffer
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
+static size_t unpack_mqtt_subscribe(const unsigned char *, union mqtt_header *,
+                                   union mqtt_packet *);
+
+/**
+ * @brief Unpacks an MQTT UNSUBSCRIBE packet from a byte buffer
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
+static size_t unpack_mqtt_unsubscribe(const unsigned char *,
+                                     union mqtt_header *, union mqtt_packet *);
+
+/**
+ * @brief Unpacks an MQTT acknowledgment packet (PUBACK, PUBREC, etc.) from a byte buffer
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
+static size_t unpack_mqtt_ack(const unsigned char *, union mqtt_header *,
+                             union mqtt_packet *);
+
+/**
+ * @brief Packs an MQTT header into a byte buffer
+ *
+ * @param[in] hdr Pointer to the MQTT header to pack
+ * @return Pointer to the packed header buffer (must be freed by caller)
+ */
+static unsigned char *pack_mqtt_header(const union mqtt_header *);
+
+/**
+ * @brief Packs an MQTT acknowledgment packet into a byte buffer
+ *
+ * @param[in] pkt Pointer to the packet to pack
+ * @return Pointer to the packed packet buffer (must be freed by caller)
+ */
+static unsigned char *pack_mqtt_ack(const union mqtt_packet *);
+
+/**
+ * @brief Packs an MQTT CONNACK packet into a byte buffer
+ *
+ * @param[in] pkt Pointer to the packet to pack
+ * @return Pointer to the packed packet buffer (must be freed by caller)
+ */
+static unsigned char *pack_mqtt_connack(const union mqtt_packet *);
+
+/**
+ * @brief Packs an MQTT SUBACK packet into a byte buffer
+ *
+ * @param[in] pkt Pointer to the packet to pack
+ * @return Pointer to the packed packet buffer (must be freed by caller)
+ */
+static unsigned char *pack_mqtt_suback(const union mqtt_packet *);
+
+/**
+ * @brief Packs an MQTT PUBLISH packet into a byte buffer
+ *
+ * @param[in] pkt Pointer to the packet to pack
+ * @return Pointer to the packed packet buffer (must be freed by caller)
+ */
+static unsigned char *pack_mqtt_publish(const union mqtt_packet *);
+/**@}*/
+
+/** @name Constants and helper functions for length encoding/decoding */
+/**@{*/
+/**
+ * @brief Maximum number of bytes for the Remaining Length field in MQTT packet header
+ *
+ * According to the MQTT v3.1.1 standard, the Remaining Length field can be at
  * most 4 bytes.
  */
 static const int MAX_LEN_BYTES = 4;
 
-/*
- * Encode Remaining Length on a MQTT packet header, comprised of Variable
- * Header and Payload if present. It does not take into account the bytes
- * required to store itself. Refer to MQTT v3.1.1 algorithm for the
- * implementation.
+/**
+ * @brief Encodes the Remaining Length field of an MQTT packet header
+ *
+ * This function implements the MQTT v3.1.1 algorithm for encoding the Remaining Length,
+ * which is the length of the Variable Header and Payload combined. It does not account
+ * for the bytes required to store the length itself.
+ *
+ * @param[out] buf Buffer to store the encoded length
+ * @param[in] len Length value to encode
+ * @return Number of bytes used for encoding the length
  */
 int mqtt_encode_length(unsigned char *buf, size_t len)
 {
-	int bytes = 0;
-	do {
-		if (bytes + 1 > MAX_LEN_BYTES)
-			return bytes;
-		short d = len % 128;
-		len /= 128;
-		/* if there are more digits to encode, set the top bit of this digit */
-		if (len > 0)
-			d |= 128;
-		buf[bytes++] = d;
-	} while (len > 0);
-	return bytes;
+    int bytes = 0;
+    do {
+        if (bytes + 1 > MAX_LEN_BYTES)
+            return bytes;
+        short d = len % 128;
+        len /= 128;
+        if (len > 0)
+            d |= 128;
+        buf[bytes++] = d;
+    } while (len > 0);
+    return bytes;
 }
 
-/*
- * Decode Remaining Length comprised of Variable Header and Payload if
- * present. It does not take into account the bytes for storing length. Refer
- * to MQTT v3.1.1 algorithm for the implementation suggestion.
+/**
+ * @brief Decodes the Remaining Length field of an MQTT packet header
  *
- * TODO Handle case where multiplier > 128 * 128 * 128
+ * This function implements the MQTT v3.1.1 algorithm for decoding the Remaining Length.
+ * It advances the buffer pointer to the next byte after the length field.
+ *
+ * @param[in,out] buf Pointer to the buffer containing the encoded length
+ * @return Decoded length value
+ * @todo Handle case where multiplier > 128 * 128 * 128
  */
 unsigned long long mqtt_decode_length(const unsigned char **buf)
 {
-	char c;
-	int multiplier = 1;
-	unsigned long long value = 0LL;
-	do {
-		c = **buf;
-		value += (c & 127) * multiplier;
-		multiplier *= 128;
-		(*buf)++;
-	} while ((c & 128) != 0);
-	return value;
+    char c;
+    int multiplier = 1;
+    unsigned long long value = 0LL;
+    do {
+        c = **buf;
+        value += (c & 127) * multiplier;
+        multiplier *= 128;
+        (*buf)++;
+    } while ((c & 128) != 0);
+    return value;
 }
+/**@}*/
 
-/*
- * MQTT unpacking functions
+/** @name MQTT packet unpacking functions */
+/**@{*/
+/**
+ * @brief Unpacks an MQTT CONNECT packet from a byte buffer
  *
-*/
+ * This function parses the CONNECT packet and fills the provided packet
+ * structure with the extracted data. It allocates memory for variable-length
+ * fields which must be freed by the caller.
+ *
+ * @param[in] buf Pointer to the buffer containing the packet
+ * @param[in] hdr Pointer to the parsed packet header
+ * @param[out] pkt Pointer to store the unpacked packet
+ * @return Size of the unpacked packet in bytes
+ */
 static size_t unpack_mqtt_connect(const unsigned char *buf,
-				  union mqtt_header *hdr,
-				  union mqtt_packet *pkt)
+                                 union mqtt_header *hdr,
+                                 union mqtt_packet *pkt)
 {
-	struct mqtt_connect connect = { .header = *hdr };
-	pkt->connect = connect;
-	const unsigned char *init = buf;
-	/*
-     * Second byte of the fixed header, contains the length of remaining bytes
-     * of the connect packet
-     */
-	size_t len = mqtt_decode_length(&buf);
-	/*
-     * For now we ignore checks on protocol name and reserved bits, just skip
-     * to the 8th byte
-     */
-	buf = init + 8;
-	/* Read variable header byte flags */
-	pkt->connect.byte = unpack_u8((const uint8_t **)&buf);
-	/* Read keepalive MSB and LSB (2 bytes word) */
-	pkt->connect.payload.keepalive = unpack_u16((const uint8_t **)&buf);
-	/* Read CID length (2 bytes word) */
-	uint16_t cid_len = unpack_u16((const uint8_t **)&buf);
-	/* Read the client id */
-	if (cid_len > 0) {
-		pkt->connect.payload.client_id = malloc(cid_len + 1);
-		unpack_bytes((const uint8_t **)&buf, cid_len,
-			     pkt->connect.payload.client_id);
-	}
-	/* Read the will topic and message if will is set on flags */
-	if (pkt->connect.bits.will == 1) {
-		unpack_string16((uint8_t **)&buf,
-				&pkt->connect.payload.will_topic);
-		unpack_string16((uint8_t **)&buf,
-				&pkt->connect.payload.will_message);
-	}
-	/* Read the username if username flag is set */
-	if (pkt->connect.bits.username == 1)
-		unpack_string16((uint8_t **)&buf,
-				&pkt->connect.payload.username);
-	/* Read the password if password flag is set */
-	if (pkt->connect.bits.password == 1)
-		unpack_string16((uint8_t **)&buf,
-				&pkt->connect.payload.password);
-	return len;
+    struct mqtt_connect connect = { .header = *hdr };
+    pkt->connect = connect;
+    const unsigned char *init = buf;
+    size_t len = mqtt_decode_length(&buf);
+    buf = init + 8;
+    pkt->connect.byte = unpack_u8((const uint8_t **)&buf);
+    pkt->connect.payload.keepalive = unpack_u16((const uint8_t **)&buf);
+    uint16_t cid_len = unpack_u16((const uint8_t **)&buf);
+    if (cid_len > 0) {
+        pkt->connect.payload.client_id = malloc(cid_len + 1);
+        unpack_bytes((const uint8_t **)&buf, cid_len,
+                    pkt->connect.payload.client_id);
+    }
+    if (pkt->connect.bits.will == 1) {
+        unpack_string16((uint8_t **)&buf,
+                       &pkt->connect.payload.will_topic);
+        unpack_string16((uint8_t **)&buf,
+                       &pkt->connect.payload.will_message);
+    }
+    if (pkt->connect.bits.username == 1)
+        unpack_string16((uint8_t **)&buf,
+                       &pkt->connect.payload.username);
+    if (pkt->connect.bits.password == 1)
+        unpack_string16((uint8_t **)&buf,
+                       &pkt->connect.payload.password);
+    return len;
 }
